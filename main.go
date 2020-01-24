237f68b57e6a72e6d13b906dfd82fb5e74ef7817
Alejandro JNM <alejandrojnm@gmail.com>
2020-01-24 12:33:01 -0500

    - Rename provider folder to civo - Change .travis.yml to use Go 1.13
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/civo/terraform-provider-civo/provider"
+	"github.com/civo/terraform-provider-civo/civo"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -9,7 +9,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return provider.Provider()
+			return civo.Provider()
 		},
 	})
 }
