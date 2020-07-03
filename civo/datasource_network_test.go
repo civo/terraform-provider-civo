439c0d42a83b8264f0d6eb018715f81ab16932dd
Alejandro JNM <alejandrojnm@gmail.com>
2020-07-03 11:03:25 -0400

    Added new data source test
    Added the load balancer test
    Added the network test
    
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

---
+++ civo/datasource_network_test.go
@@ -0,0 +1,39 @@
+package civo
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourceCivoNetwork_basic(t *testing.T) {
+	datasourceName := "data.civo_network.foobar"
+	name := acctest.RandomWithPrefix("net-test")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceCivoNetworkConfig(name),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(datasourceName, "label", name),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceCivoNetworkConfig(name string) string {
+	return fmt.Sprintf(`
+resource "civo_network" "foobar" {
+	label = "%s"
+}
+
+data "civo_network" "foobar" {
+	label = civo_network.foobar.name
+}
+`, name)
+}
