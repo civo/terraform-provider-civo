abff9aa50876d26ad64e010a99d7cf232ac6a70a
Zulhilmi Zainudin <zulh@civo.com>
2021-08-27 15:24:11 +0800

    Save 'firewall_id' to state and add validations for it

--- civo/resource_kubernetes_cluster.go
+++ civo/resource_kubernetes_cluster.go
@@ -283,7 +283,17 @@ func resourceKubernetesClusterCreate(d *schema.ResourceData, m interface{}) erro
 	}
 
 	if attr, ok := d.GetOk("firewall_id"); ok {
-		config.InstanceFirewall = attr.(string)
+		firewallID := attr.(string)
+		firewall, err := apiClient.FindFirewall(firewallID)
+		if err != nil {
+			return fmt.Errorf("[ERR] unable to find firewall - %s", err)
+		}
+
+		if firewall.NetworkID != config.NetworkID {
+			return fmt.Errorf("[ERR] firewall %s is not part of network %s", firewall.ID, config.NetworkID)
+		}
+
+		config.InstanceFirewall = firewallID
 	}
 
 	log.Printf("[INFO] creating a new kubernetes cluster %s", d.Get("name").(string))
@@ -353,6 +363,7 @@ func resourceKubernetesClusterRead(d *schema.ResourceData, m interface{}) error 
 	d.Set("dns_entry", resp.DNSEntry)
 	// d.Set("built_at", resp.BuiltAt.UTC().String())
 	d.Set("created_at", resp.CreatedAt.UTC().String())
+	d.Set("firewall_id", resp.FirewallID)
 
 	if err := d.Set("instances", flattenInstances(resp.Instances)); err != nil {
 		return fmt.Errorf("[ERR] error retrieving the instances for kubernetes cluster error: %#v", err)
@@ -379,6 +390,14 @@ func resourceKubernetesClusterUpdate(d *schema.ResourceData, m interface{}) erro
 	}
 
 	config := &civogo.KubernetesClusterConfig{}
+
+	if d.HasChange("network_id") {
+		return fmt.Errorf("[ERR] Network change (%q) for existing cluster is not available at this moment", "network_id")
+	}
+
+	if d.HasChange("firewall_id") {
+		return fmt.Errorf("[ERR] Firewall change (%q) for existing cluster is not available at this moment", "firewall_id")
+	}
 
 	if d.HasChange("target_nodes_size") {
 		errMsg := []string{
