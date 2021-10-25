a0f919e9487e71bedc16363f5e41f80db94cbc8f
James Gawn <james@gawn.uk>
2021-10-25 08:32:54 +0100

    Add missing required provider section in terraform docs provider example (#101)
    * Add required provider block to provider example
    
    * Adjust comment

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,6 +1,15 @@
 # Set the variable value in *.tfvars file or using -var="civo_token=..." CLI flag
 variable "civo_token" {}
 
+# Specify required provider as maintained by civo
+terraform {
+  required_providers {
+    civo = {
+      source = "civo/civo"
+    }
+  }
+}
+
 # Configure the Civo Provider
 provider "civo" {
   token = var.civo_token
