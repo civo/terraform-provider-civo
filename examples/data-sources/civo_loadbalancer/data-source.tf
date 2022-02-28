a06a03405dd6e78cde30dd44e84ce586a523f03f
Alejandro J. Nuñez Madrazo <alejandrojnm@gmail.com>
2022-02-28 09:23:30 +0000

    Added the lb example for the doc
    Signed-off-by: Alejandro J. Nuñez Madrazo <alejandrojnm@gmail.com>

--- examples/data-sources/civo_loadbalancer/data-source.tf
+++ examples/data-sources/civo_loadbalancer/data-source.tf
@@ -1,1 +1,9 @@
 # TODO
+data civo_loadbalancer "my-lb" {
+  #id = "c385638f-6bb7-4d74-840c-4d98f3d15082" // Optional
+  name = "lb-name"
+}
+
+output "civo_loadbalancer_output" {
+  value = data.civo_loadbalancer.my-lb.public_ip
+}
