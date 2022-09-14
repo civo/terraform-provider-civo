05568b8e0468d671db907c19c7fb39e2a9804d75
Alejandro J. Nuñez Madrazo <alejandrojnm@gmail.com>
2022-09-14 13:56:14 +0100

    remove unused import
    Signed-off-by: Alejandro J. Nuñez Madrazo <alejandrojnm@gmail.com>

--- civo/datasource_object_store.go
+++ civo/datasource_object_store.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 
 	"github.com/civo/civogo"
-	// "github.com/civo/terraform-provider-civo/internal/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
