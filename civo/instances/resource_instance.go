d2cb553d61fdb0ae1d6286fc46f64ca13abb50e7
Praveen005 <praveen08h@gmail.com>
2024-07-24 22:13:29 +0530

    fix: ensure 'instance' handles 'reserve_ipv4' field for both IP and ID correctly without constant updates

--- civo/instances/resource_instance.go
+++ civo/instances/resource_instance.go
@@ -374,7 +374,7 @@ func resourceInstanceRead(_ context.Context, d *schema.ResourceData, m interface
 	d.Set("firewall_id", resp.FirewallID)
 	d.Set("status", resp.Status)
 	d.Set("script", resp.Script)
-	d.Set("reserved_ipv4", resp.ReservedIPID)
+	// d.Set("reserved_ipv4", resp.ReservedIPID)
 	d.Set("created_at", resp.CreatedAt.UTC().String())
 	d.Set("notes", resp.Notes)
 	d.Set("disk_image", resp.SourceID)
@@ -387,6 +387,12 @@ func resourceInstanceRead(_ context.Context, d *schema.ResourceData, m interface
 
 	if _, ok := d.GetOk("template"); ok {
 		d.Set("template", d.Get("template").(string))
+	}
+
+	if d.HasChange("reserved_ipv4"){
+		_, new := d.GetChange("reserved_ipv4")
+		newValue := new.(string)
+		d.Set("reserved_ipv4", newValue)
 	}
 
 	return nil
